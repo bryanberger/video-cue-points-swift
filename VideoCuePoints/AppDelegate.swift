ed7c606c71f3d43e9c83cf70c00c8cd76429d2f0
Bryan Berger <bb@ga.co>
2016-08-25 19:30:57 -0400

    Initial Commit

---
+++ VideoCuePoints/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoCuePoints
+//
+//  Created by Bryan Berger on 8/25/16.
+//  Copyright © 2016 Bryan Berger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoCuePoints/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoCuePoints
+//
+//  Created by Bryan Berger on 8/25/16.
+//  Copyright © 2016 Bryan Berger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
